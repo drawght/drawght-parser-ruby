c8c33f32010928381347d3d8a8769502895cdb50
Hallison Batista <email@hallison.dev.br>
2024-08-29 23:17:12 -0400

    Tests using Minitest

---
+++ test/drawght_compiler_test.rb
@@ -0,0 +1,26 @@
+require "minitest/autorun"
+require_relative "../lib/drawght"
+
+describe "drawght compiler" do
+  describe "when compile" do
+    it "convert variables" do
+      skip
+    end
+
+    it "convert hash objects" do
+      skip
+    end
+
+    it "convert list" do
+      skip
+    end
+
+    it "convert item in a list" do
+      skip
+    end
+
+    it "convert list of objects" do
+      skip
+    end
+  end
+end
