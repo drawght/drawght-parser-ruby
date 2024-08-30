8c75e9a9807e32fc8d320d800b1bedf99247efaa
Hallison Batista <email@hallison.dev.br>
2024-08-29 23:20:24 -0400

    Tests for variables conversion

--- test/drawght_compiler_test.rb
+++ test/drawght_compiler_test.rb
@@ -4,7 +4,16 @@ require_relative "../lib/drawght"
 describe "drawght compiler" do
   describe "when compile" do
     it "convert variables" do
-      skip
+      template = "{name} v{version} ({release date}/{start-at})";
+      data = {
+        name: "Drawght",
+        version: "0.1.0",
+        "release date" => "2021-07-01",
+        "start-at" => "2021-06-30",
+      }
+      result = Drawght::Compiler.new(template).compile data
+
+      expect(result).must_equal "Drawght v0.1.0 (2021-07-01/2021-06-30)"
     end
 
     it "convert hash objects" do
