89cc89d071e6566b087a05c6b8e96f3f96af17e9
Hallison Batista <email@hallison.dev.br>
2024-09-05 00:41:24 -0400

    Pry for debugging

--- Gemfile
+++ Gemfile
@@ -1,7 +1,9 @@
 source 'http://rubygems.org'
 
+gemspec
+
 group :development do
   gem "minitest"
+  gem "pry"
+  gem "pry-byebug"
 end
-
-gemspec
--- lib/drawght.rb
+++ lib/drawght.rb
@@ -1,5 +1,6 @@
 module Drawght
   autoload :Compiler, "#{__dir__}/drawght/compiler"
+  autoload :Markup, "#{__dir__}/drawght/markup"
 
   def self.load(template)
     Compiler.new template
