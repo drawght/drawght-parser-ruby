6c98035e60239e840193df299dddf03b3c017c7f
Hallison Batista <email@hallison.dev.br>
2024-09-12 01:52:03 -0400

    Class refactoring for link markup

--- lib/drawght/markup/link.rb
+++ lib/drawght/markup/link.rb
@@ -4,26 +4,22 @@ module Drawght::Markup
 
 class Link
   def initialize
-    @mapping = {
-      "[" => "a",
-    }
-    @pattern = %r{
-      (?<start>\[)
-      (?<content>.*?):[ ]{1,}
-      (?<reference>.*?)
-      ([ ]{1,}(?<title>.*?))?
-      (?<finish>\])
-      (?<eol>[\s\n]?)
-    }xm
-    @tagging = '<%{tag} href="%{reference}" title="%{title}">%{content}</%{tag}>%{eol}'
+    default_mapping
+    default_expression
+    default_tagging
   end
 
   def process text
     result = text.dup
 
-    text.scan @pattern do |(start, content, reference, title, finish, eol)|
-      match = result.match @pattern
-      tagging = convert_markup_to_tag(start, content, reference:, title:, eol:)
+    text.scan @expression do |(start, content, reference, title, finish, eol)|
+      match = result.match @expression
+      tagging = markup_to_tag start, {
+        reference:,
+        title:,
+        content:,
+        eol:,
+      }
       result.gsub! match[0], tagging
     end
 
@@ -32,9 +28,33 @@ class Link
 
   private
 
-  def convert_markup_to_tag markup, content, options = {}
-    tag = @mapping.fetch markup
-    format @tagging, options.update(tag:, content:)
+  def default_mapping
+    @mapping = {
+      "[" => "a",
+    }
+  end
+
+  def default_expression
+    @expression = %r{
+      (?<start>\[)
+      (?<content>.*?):[ ]{1,}
+      (?<reference>.*?)
+      ([ ]{1,}(?<title>.*?))?
+      (?<finish>\])
+      (?<eol>[\s\n]?)
+    }xm
+  end
+
+  def default_tagging
+    @tagging = '<%{tag} href="%{reference}" title="%{title}">%{content}</%{tag}>%{eol}'
+  end
+
+  def markup_to_tag markup, properties = {}
+    format @tagging, properties.update(tag: tag_for(markup))
+  end
+
+  def tag_for markup
+    @mapping.fetch markup
   end
 end
 
